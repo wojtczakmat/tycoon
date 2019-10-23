3f88ad3d1f8cb6ff7807426a59d64b5e652c1667
Mateusz Wojtczak <mateusz.wojtczak@leancode.pl>
2019-10-24 01:33:39 +0200

    Fix root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'status/index'
+  
+  root 'status#index'
 end
